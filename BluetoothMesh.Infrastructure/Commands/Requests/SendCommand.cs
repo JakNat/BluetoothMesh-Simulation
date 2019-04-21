17d48288cfe5192c813c651eb632759f7e504f8e
kuba.natonek998 <kuba.natonek998@gmail.com>
2019-04-21 14:06:53 +0200

    Added SendCommand and his handler

---
+++ BluetoothMesh.Infrastructure/Commands/Requests/SendCommand.cs
@@ -0,0 +1,17 @@
+﻿using BluetoothMesh.Core.Domain;
+using BluetoothMesh.Core.Domain.Requests;
+
+namespace BluetoothMesh.Infrastructure.Commands.Requests
+{
+    public class SendCommand : ICommand
+    {
+        public SendCommand(BaseNode node, BaseRequest baseRequest)
+        {
+            Node = node;
+            BaseRequest = baseRequest;
+        }
+
+        public BaseNode Node { get; set; }
+        public BaseRequest BaseRequest { get; set; }
+    }
+}
---
+++ BluetoothMesh.Infrastructure/Handler/SendHandler.cs
@@ -0,0 +1,20 @@
+﻿using BluetoothMesh.Infrastructure.Commands;
+using BluetoothMesh.Infrastructure.Commands.Requests;
+using BluetoothMesh.Infrastructure.Services;
+
+namespace BluetoothMesh.Infrastructure.Handler
+{
+    public class SendHandler : ICommandHandler<SendCommand>
+    {
+        private readonly IBroadcastService _broadcastService;
+
+        public SendHandler(IBroadcastService broadcastService)
+        {
+            _broadcastService = broadcastService;
+        }
+        public void Handle(SendCommand command)
+        {
+            _broadcastService.SendBroadcast(command.Node,command.BaseRequest);
+        }
+    }
+}
