25ce5576b0098d9703fe5510bf66ce8a02b7632e
kuba.natonek998 <kuba.natonek998@gmail.com>
2019-04-21 14:15:28 +0200

    Implement dependency injection using Autofac

--- BluetoothMesh/Program.cs
+++ BluetoothMesh/Program.cs
@@ -1,26 +1,30 @@
-﻿using BluetoothMesh.Core.Domain;
+﻿using Autofac;
+using BluetoothMesh.Core.Domain;
 using BluetoothMesh.Core.Domain.Requests;
+using BluetoothMesh.Core.Repositories;
+using BluetoothMesh.Infrastructure.Commands;
 using BluetoothMesh.Infrastructure.Configuration;
 using BluetoothMesh.Infrastructure.DBL;
 using BluetoothMesh.Infrastructure.Repositories;
 using BluetoothMesh.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace BluetoothMesh
 {
     class Program
     {
+        static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
-            var context = new BluetoothMeshContext();
-            var nodeRepository = new BaseNodeRepository<BaseNode>(context);
-            var broadcastService = new BroadcastService(nodeRepository);
+            Container = BuildContainer();
+            var context = Container.Resolve<IBluetoothMeshContext>();
 
-            foreach (var nodeServer in context.NodeServers)
+            foreach (var server in context.NodeServers)
             {
-                nodeServer.SetBroadCastService(broadcastService);
-                nodeServer.RegisterBasicResponse();
+                server.SetDispacher(Container.Resolve<ICommandDispatcher>());
             }
 
             // testowy request 
@@ -36,6 +40,29 @@ namespace BluetoothMesh
             Console.WriteLine("\nPress any key to close server.");
             Console.ReadKey(true);
 
+        }
+
+        static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<BaseNodeRepository<BaseNode>>().As<IBaseNodeRepository<BaseNode>>();
+            builder.RegisterType<BroadcastService>().As<IBroadcastService>();
+
+            builder.RegisterType<BluetoothMeshContext>().As<IBluetoothMeshContext>().SingleInstance();
+
+            var assembly = typeof(ICommand)
+                .GetTypeInfo()
+                .Assembly;
+
+            builder.RegisterAssemblyTypes(assembly)
+                   .AsClosedTypesOf(typeof(ICommandHandler<>))
+                   .InstancePerLifetimeScope();
+
+            builder.RegisterType<CommandDispatcher>()
+                .As<ICommandDispatcher>()
+                .InstancePerLifetimeScope();
+
+            return builder.Build();
         }
     }
 }
