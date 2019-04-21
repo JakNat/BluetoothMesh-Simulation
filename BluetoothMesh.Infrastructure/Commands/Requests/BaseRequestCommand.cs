4218a11bbfc1f557efe84bfe1cd8984034c0a229
kuba.natonek998 <kuba.natonek998@gmail.com>
2019-04-21 14:06:35 +0200

    Added BaseRequestCommand and his handler

---
+++ BluetoothMesh.Infrastructure/Commands/Requests/BaseRequestCommand.cs
@@ -0,0 +1,18 @@
+﻿using BluetoothMesh.Core.Domain.Requests;
+using BluetoothMesh.Infrastructure.Configuration;
+
+namespace BluetoothMesh.Infrastructure.Commands.Requests
+{
+    public class BaseRequestCommand : ICommand
+    {
+        public BaseRequestCommand(NodeServer nodeServer, BaseRequest incomingObject)
+        {
+            NodeServer = nodeServer;
+            IncomingObject = incomingObject;
+        }
+
+        public NodeServer NodeServer{ get; set; }
+        public BaseRequest IncomingObject{ get; set; }
+        
+    }
+}
---
+++ BluetoothMesh.Infrastructure/Handler/BaseRequestHandler.cs
@@ -0,0 +1,47 @@
+﻿using BluetoothMesh.Infrastructure.Commands;
+using BluetoothMesh.Infrastructure.Commands.Requests;
+using BluetoothMesh.Infrastructure.Services;
+using System;
+
+namespace BluetoothMesh.Infrastructure.Handler
+{
+    class BaseRequestHandler : ICommandHandler<BaseRequestCommand>
+    {
+        private readonly IBroadcastService _broadcastService;
+
+        public BaseRequestHandler(IBroadcastService broadcastService)
+        {
+            _broadcastService = broadcastService;
+        }
+        public void Handle(BaseRequestCommand command)
+        {
+            var incomingObject = command.IncomingObject;
+            var nodeServer = command.NodeServer;
+              if (nodeServer.ReceivedRequests.Contains(incomingObject.RequestId))
+            {
+                Console.WriteLine("Node nr " + nodeServer.Node.Id + " blocked from nr " + incomingObject.BroadCastingNodeId);
+                return ;
+            }
+
+            nodeServer.ReceivedRequests.Add(incomingObject.RequestId);
+
+            Console.WriteLine("Node nr " + nodeServer.Node.Id + " get message from {Node " + incomingObject.BroadCastingNodeId + "}");
+            if (incomingObject.TargetNodeId == nodeServer.Node.Id)
+            {
+                Console.WriteLine("CONGRATS!!");
+                return ;
+            }
+
+            if (incomingObject.Heartbeats == 0)
+            {
+                Console.WriteLine("Message died :<");
+                return ;
+            }
+
+            var newRequest = incomingObject;
+            newRequest.Heartbeats--;
+            newRequest.BroadCastingNodeId = nodeServer.Node.Id;
+            _broadcastService.SendBroadcast(nodeServer.Node, newRequest);
+        }
+    }
+}
